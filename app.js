246754f105aff7ab61ed8a2028e3cb2deac060dd
BHAVANIM <bhavanimbhavanim65@gmail.com>
2025-10-28 23:58:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("BHAVANI MUNIRAJU")
+
+
+var name ="bhavani m"
+console.log(name)
+var name = "Shivani m"
+console.log(name)
+
+
+//data types
+name = "BHAVANI M"
+console.log(typeof name)//output:-string
+age = 111
+console.log(typeof  age)   //number
+marks= 85.5
+console.log(typeof marks)  //MARKS
+isstudent = true
+console.log(typeof isstudent) //boolean
+word = null
+console.log(typeof  word)//object
+// what;
+// console.log(typeof what)
+
+alert("THIS IS MY 2ND DAY OF LEARNING JAVASCRIPT 💥")
