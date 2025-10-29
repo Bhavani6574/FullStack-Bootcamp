aae941e3fd8a72861de068a6cd9872aeac79c7dd
BHAVANIM <bhavanimbhavanim65@gmail.com>
2025-10-29 23:57:00 +0530

    Add files via upload

---
+++ day3.2.js
@@ -0,0 +1,53 @@
+let name = "BHAVANI M";
+let age = 16;
+let goal = "FULL STACK DEV -> AI -> QUANTUM";
+console.log(`I'M ${name}, ${age} years old , I WANT TO BE NO!! I WILL BE ${goal}`);
+
+let a = '2';
+let b = '3';
+console.log(a + b);
+
+let x = 10;
+let y = "10";
+let z;
+let t = null;
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof t);
+
+let name2 = "Bhavani m";
+let age2 = 16;
+let goal2 = "full - ai - quantum";
+console.log(`I'm ${name2}, ${age2} years old , i want to be ${goal2}`);
+
+let num = 42;
+let text = "Hello";
+let isCool = true;
+let nothing = null;
+let notDefined;
+
+console.log(typeof num);
+console.log(typeof text);
+console.log(typeof isCool);
+console.log(typeof nothing);
+console.log(typeof notDefined);
+
+let firstName = "Bhavani";
+let lastName = "M";
+let goal3 = "Full Stack → AI → Quantum Developer";
+console.log(`HIi, I'm ${firstName} ${lastName}, and my dream is to become a ${goal3}.`);
+
+let a2 = 5;
+let b2 = 3;
+console.log("sum:", a2 + b2);
+console.log("difference:", a2 - b2);
+console.log("multiplication:", a2 * b2);
+console.log("division:", a2 / b2);
+console.log("remainder:", a2 % b2);
+
+let x2 = 5;
+let y2 = "5";
+console.log(x2 == y2);
+console.log(x2 === y2);
