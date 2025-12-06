3936aa445bc905e4ac62273bb6ad5263979295b4
BHAVANIM <bhavanimbhavanim65@gmail.com>
2025-12-06 23:48:40 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,38 @@
+//Print 1 to 10
+let i;
+for (i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+
+
+
+//print 10 to 1 (reverse)
+let n;
+for (n = 10; n >= 1; n--) {
+  console.log(n);
+}
+
+
+//Even numbers 2 to 20
+
+let l;
+for (l = 2; l <= 20; l += 2) {
+  console.log(i);
+}
+
+//Odd numbers 1 to 19
+let a;
+for (a = 1; a <= 19; a += 2) {
+  console.log(a);
+}
+
+
+//Multiples of 5 (5 to 50)
+
+let m;
+for (m = 5; m <= 50; m += 5) {
+  console.log(a);
+}
+
+
