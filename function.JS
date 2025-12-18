1e3d261473bcbd20b257831beb6db7bcbe00744c
BHAVANIM <bhavanimbhavanim65@gmail.com>
2025-12-18 16:39:47 +0530

    Add files via upload

---
+++ function.JS
@@ -0,0 +1,105 @@
+let a = "hi";
+let b = " miki"
+
+function greet()
+{
+  let c;
+  c = a + b;
+  console.log(c);
+}
+greet();
+
+
+
+let a1 = "hi";
+let b2 = " miki"
+
+function greet()
+{
+  let c1;
+  c1 = a1 + b1;
+  return(c);
+}
+let res = greet();
+
+console.log(res)
+
+
+
+
+
+function greet(a1 ,b2) {
+  let c2;
+  c2 = a2 + b;
+  return c;
+}
+let resa =greet(100,333);
+console.log(res);
+
+
+
+function area(a,b)
+{
+    let c; 
+    c = a * b;
+    return c
+}
+console.log(area(20,20));
+console.log(area(30, 30));
+
+
+
+
+
+
+
+function maximum(a,b)
+{
+ return Math.max(a,b)
+}
+console.log(maximum(22.1,22))
+
+
+
+
+let evenorodd = function(a)
+{
+  if (a%2==0){
+    console.log(`It is an even `)
+  }
+  else{
+    console.log(`It is an odd`);
+  }
+}
+console.log(evenorodd(7))
+
+
+
+
+ let evenorodd1 = function () {
+   if (evenorodd1 % 2 == 0) {
+     return(`It is an even `);
+   } else {
+     return(`It is an odd`);
+   }
+ };
+ console.log(evenorodd1(2))
+
+
+ 
+
+ let average = (a,b,c) => (a+b+c)/3
+ console.log(average(30,20,10));
+
+ 
+
+
+ (function()
+{
+  console.log("App started")
+})();
+
+
+
+let square = (n) => n*n;
+console.log(square(20,20));
