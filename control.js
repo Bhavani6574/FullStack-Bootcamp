ddf5d5ccb899e853a0263cd2bc663dbccb2bf974
BHAVANIM <bhavanimbhavanim65@gmail.com>
2025-12-20 23:44:09 +0530

    Update and rename day4.js to control.js

---
+++ control.js
@@ -0,0 +1,33 @@
+                             
+
+console.log("----- CONDITIONAL STATEMENTS -----");
+
+let marks = 85;
+
+if (marks >= 90) {
+  console.log("Grade: A+");
+} else if (marks >= 75) {
+  console.log("Grade: A");
+} else if (marks >= 60) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: Fail");
+}
+
+let day = 3;
+switch (day) {
+  case 1: console.log("Monday"); break;
+  case 2: console.log("Tuesday"); break;
+  case 3: console.log("Wednesday"); break;
+  case 4: console.log("Thursday"); break;
+  case 5: console.log("Friday"); break;
+  case 6: console.log("Saturday"); break;
+  case 7: console.log("Sunday"); break;
+  default: console.log("Invalid Day");
+}
+
+
+
+
+
+
